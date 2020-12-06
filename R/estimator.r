8b2b5e64230803a96f9eb8413cb909ffc8e2286f
Samuel Gailliot <samuel.gailliot@stat.tamu.edu>
2020-12-06 14:59:28 -0600

    Redid rd_estimate file and added documentation.

--- R/estimator.r
+++ R/estimator.r
@@ -1,227 +1,78 @@
-estimator <- function (formula, data, subset = NULL, cutpoint = NULL, bw = NULL,
-          kernel = "triangular", se.type = "HC1", cluster = NULL, verbose = FALSE,
-          model = FALSE, frame = FALSE)
-{
-  call <- match.call()
-  if (missing(data))
-    data <- environment(formula)
-  formula <- as.Formula(formula)
-  X <- model.frame(formula, rhs = 1, lhs = 0, data = data,
-                   na.action = na.pass)[[1]]
-  Y <- model.frame(formula, rhs = 0, lhs = NULL, data = data,
-                   na.action = na.pass)[[1]]
-  if (!is.null(subset)) {
-    X <- X[subset]
-    Y <- Y[subset]
-    if (!is.null(cluster))
-      cluster <- cluster[subset]
-  }
-  if (!is.null(cluster)) {
-    cluster <- as.character(cluster)
-    robust.se <- function(model, cluster) {
-      M <- length(unique(cluster))
-      N <- length(cluster)
-      K <- model$rank
-      dfc <- (M/(M - 1)) * ((N - 1)/(N - K))
-      uj <- apply(estfun(model), 2, function(x) tapply(x,
-                                                       cluster, sum))
-      rcse.cov <- dfc * sandwich(model, meat. = crossprod(uj)/N)
-      rcse.se <- coeftest(model, rcse.cov)
-      return(rcse.se[2, 2])
-    }
-  }
-  na.ok <- complete.cases(X) & complete.cases(Y)
-  if (length(all.vars(formula(formula, rhs = 1, lhs = F))) >
-      1) {
-    type <- "fuzzy"
-    Z <- model.frame(formula, rhs = 1, lhs = 0, data = data,
-                     na.action = na.pass)[[2]]
-    if (!is.null(subset))
-      Z <- Z[subset]
-    na.ok <- na.ok & complete.cases(Z)
-    if (length(all.vars(formula(formula, rhs = 1, lhs = F))) >
-        2)
-      stop("Invalid formula. Read ?RDestimate for proper syntax")
-  }
-  else {
-    type = "sharp"
-  }
-  covs <- NULL
-  if (length(formula)[2] > 1) {
-    covs <- model.frame(formula, rhs = 2, lhs = 0, data = data,
-                        na.action = na.pass)
-    if (!is.null(subset))
-      covs <- subset(covs, subset)
-    na.ok <- na.ok & complete.cases(covs)
-    covs <- subset(covs, na.ok)
-  }
-  X <- X[na.ok]
-  Y <- Y[na.ok]
-  if (type == "fuzzy")
-    Z <- as.double(Z[na.ok])
-  if (is.null(cutpoint)) {
-    cutpoint <- 0
-    if (verbose)
-      cat("No cutpoint provided. Using default cutpoint of zero.\n")
-  }
-  if (frame) {
-    if (type == "sharp") {
-      if (!is.null(covs))
-        dat.out <- data.frame(X, Y, covs)
-      else dat.out <- data.frame(X, Y)
-    }
-    else {
-      if (!is.null(covs))
-        dat.out <- data.frame(X, Y, Z, covs)
-      else dat.out <- data.frame(X, Y, Z)
-    }
-  }
-  if (is.null(bw)) {
-    bw <- IKbandwidth(X = X, Y = Y, cutpoint = cutpoint,
-                      kernel = kernel, verbose = verbose)
-    bws <- c(bw, 0.5 * bw, 2 * bw)
-    names(bws) <- c("LATE", "Half-BW", "Double-BW")
-  }
-  else if (length(bw) == 1) {
-    bws <- c(bw, 0.5 * bw, 2 * bw)
-    names(bws) <- c("LATE", "Half-BW", "Double-BW")
-  }
-  else {
-    bws <- bw
-  }
-  o <- list()
-  o$type <- type
-  o$call <- call
-  o$est <- vector(length = length(bws), mode = "numeric")
-  names(o$est) <- names(bws)
-  o$bw <- as.vector(bws)
-  o$se <- vector(mode = "numeric")
-  o$z <- vector(mode = "numeric")
-  o$p <- vector(mode = "numeric")
-  o$obs <- vector(mode = "numeric")
-  o$ci <- matrix(NA, nrow = length(bws), ncol = 2)
-  o$model <- list()
-  if (type == "fuzzy") {
-    o$model$firststage <- list()
-    o$model$iv <- list()
-  }
-  o$frame <- list()
-  o$na.action <- which(na.ok == FALSE)
-  class(o) <- "RD"
-  X <- X - cutpoint
-  Xl <- (X < 0) * X
-  Xr <- (X >= 0) * X
-  Tr <- as.integer(X >= 0)
-  for (bw in bws) {
-    ibw <- which(bw == bws)
-    sub <- X >= (-bw) & X <= (+bw)
-    if (kernel == "gaussian")
-      sub <- TRUE
-    w <- kernelwts(X, 0, bw, kernel = kernel)
-    o$obs[ibw] <- sum(w > 0)
-    if (type == "sharp") {
-      if (verbose) {
-        cat("Running Sharp RD\n")
-        cat("Running variable:", all.vars(formula(formula,
-                                                  rhs = 1, lhs = F))[1], "\n")
-        cat("Outcome variable:", all.vars(formula(formula,
-                                                  rhs = F, lhs = 1))[1], "\n")
-        if (!is.null(covs))
-          cat("Covariates:", paste(names(covs), collapse = ", "),
-              "\n")
-      }
-      if (!is.null(covs)) {
-        data <- data.frame(Y, Tr, Xl, Xr, covs, w)
-        form <- as.formula(paste("Y~Tr+Xl+Xr+", paste(names(covs),
-                                                      collapse = "+", sep = ""), sep = ""))
-      }
-      else {
-        data <- data.frame(Y, Tr, Xl, Xr, w)
-        form <- as.formula(Y ~ Tr + Xl + Xr)
-      }
-      mod <- lm(form, weights = w, data = subset(data,
-                                                 w > 0))
-      if (verbose == TRUE) {
-        cat("Model:\n")
-        print(summary(mod))
-      }
-      o$est[ibw] <- coef(mod)["Tr"]
-      if (is.null(cluster)) {
-        o$se[ibw] <- coeftest(mod, vcovHC(mod, type = se.type))[2,
-                                                                2]
-      }
-      else {
-        o$se[ibw] <- robust.se(mod, cluster[na.ok][w >
-                                                     0])
-      }
-      o$z[ibw] <- o$est[ibw]/o$se[ibw]
-      o$p[ibw] <- 2 * pnorm(abs(o$z[ibw]), lower.tail = F)
-      o$ci[ibw, ] <- c(o$est[ibw] - qnorm(0.975) * o$se[ibw],
-                       o$est[ibw] + qnorm(0.975) * o$se[ibw])
-      if (model)
-        o$model[[ibw]] = mod
-      if (frame)
-        o$frame[[ibw]] = dat.out
-    }
-    else {
-      if (verbose) {
-        cat("Running Fuzzy RD\n")
-        cat("Running variable:", all.vars(formula(formula,
-                                                  rhs = 1, lhs = F))[1], "\n")
-        cat("Outcome variable:", all.vars(formula(formula,
-                                                  rhs = F, lhs = 1))[1], "\n")
-        cat("Treatment variable:", all.vars(formula(formula,
-                                                    rhs = 1, lhs = F))[2], "\n")
-        if (!is.null(covs))
-          cat("Covariates:", paste(names(covs), collapse = ", "),
-              "\n")
-      }
-      if (!is.null(covs)) {
-        data <- data.frame(Y, Tr, Xl, Xr, Z, covs, w)
-        form <- as.Formula(paste("Y~Z+Xl+Xr+", paste(names(covs),
-                                                     collapse = "+"), "|Tr+Xl+Xr+", paste(names(covs),
-                                                                                          collapse = "+"), sep = ""))
-        form1 <- as.Formula(paste("Z~Tr+Xl+Xr+", paste(names(covs),
-                                                       collapse = "+", sep = "")))
-      }
-      else {
-        data <- data.frame(Y, Tr, Xl, Xr, Z, w)
-        form <- as.Formula(Y ~ Z + Xl + Xr | Tr + Xl +
-                             Xr)
-        form1 <- as.formula(Z ~ Tr + Xl + Xr)
-      }
-      mod1 <- lm(form1, weights = w, data = subset(data,
-                                                   w > 0))
-      mod <- ivreg(form, weights = w, data = subset(data,
-                                                    w > 0))
-      if (verbose == TRUE) {
-        cat("First stage:\n")
-        print(summary(mod1))
-        cat("IV-RD:\n")
-        print(summary(mod))
-      }
-      o$est[ibw] <- coef(mod)["Z"]
-      if (is.null(cluster)) {
-        o$se[ibw] <- coeftest(mod, vcovHC(mod, type = se.type))[2,
-                                                                2]
-      }
-      else {
-        o$se[ibw] <- robust.se(mod, cluster[na.ok][w >
-                                                     0])
-      }
-      o$z[ibw] <- o$est[ibw]/o$se[ibw]
-      o$p[ibw] <- 2 * pnorm(abs(o$z[ibw]), lower.tail = F)
-      o$ci[ibw, ] <- c(o$est[ibw] - qnorm(0.975) * o$se[ibw],
-                       o$est[ibw] + qnorm(0.975) * o$se[ibw])
-      if (model) {
-        o$model$firststage[[ibw]] <- mod1
-        o$model$iv[[ibw]] = mod
-      }
-      if (frame)
-        o$frame = dat.out
-    }
-  }
-  return(o)
-}
+#' @title RD Estimation with Robust Confidence Intervals
+#'
+#' @description \code{rd_estimate} calls the powerful function rdrobust from the rdrobust package but makes only the most important options available to the user to make the function easier to use. This function only performs local linear regression to construct the point-estimator.
+#' @param Y Nx1 vector containing dependent variable
+#' @param X Nx1 vector containing running variable (forcing variable)
+#' @param cutpoint Location of discontinuity.
+#' @param bandwidth Specified the main bandwidth used to construct the RD point estimator. If not specified, bandwidth is computed by the companion command rdbwselect from rdrobust package.
+#' @param kernel_method The kernel function used to construct the local-polynomial estimators. Options are \code{triangular} (default), \code{epanechnikov}, and \code{uniform}.
+#' @param bw_method specifies the bandwidth selection to be used. By defauslt it computes both main (h) and bias (b). Options are:
+#'
+#' \code{mserd} one common MSE-optimal bandwidth selector for the RD treatment effect estimator.
+#'
+#' \code{msetwo} two different MSE-optimal bandwidth selectors (below and above the cut point) for the RD treatment effect estimator.
+#'
+#' \code{msesum} one common MSE-optimal bandwidth selector for the sum of regression estimates.
+#'
+#' \code{msecomb1} for \code{min(mserd, msesum)}.
+#'
+#' \code{mescomb2} for \code{median(nsetwo, mserd, msesum)}, for each side of the cutoff separately.
+#'
+#' \code{cerrd} one common CER-optimal bandwidth selector for the RD treatment effect estimator.
+#'
+#' \code{certwo} two different CER-optimal bandwidth selectors (below and above the cutoff) for the RD treatment effect estimator
+#'
+#' \code{cersum} one common CER-optimal bandwidth selector for the sum of regression estimates (as opposed to difference therof).
+#'
+#' \code{cercomb1} for \code{min(cerrd, cersum)}
+#'
+#' \code{cercomb2} for \code{median(certwo, cerrd, cerdsum)} for each side of the cutoff seperately.
+#'
+#' @param cl The confidence lever for confidence intervals. The default is cl = 95.
+#'
+#' @return Named list of outputs.
+#'
+#' @author Samuel Gailliot, Texas A (and) M University, College Station, TX. samuel.gailliot@stat.tamu.edu
+#'
+#' @references Calonico, S., M. D. Cattaneo, and M. H. Farrell. 2018. On the Effect of Bias Estimation on Coverage Accuracy in Nonparametric Inference. Journal of the American Statistical Association, 113(522): 767-779.
+#'
+#' Calonico, S., M. D. Cattaneo, and M. H. Farrell. 2020. Optimal Bandwidth Choice for Robust Bias Corrected Inference in Regression Discontinuity Designs. Econometrics Journal, 23(2): 192-210.
+#'
+#' Calonico, S., M. D. Cattaneo, M. H. Farrell, and R. Titiunik. 2017. rdrobust: Software for Regression Discontinuity Designs. Stata Journal, 17(2): 372-404.
+#'
+#' Calonico, S., M. D. Cattaneo, M. H. Farrell, and R. Titiunik. 2019. Regression Discontinuity Designs using Covariates. Review of Economics and Statistics, 101(3): 442-451.
+#'
+#' Calonico, S., M. D. Cattaneo, and R. Titiunik. 2014a. Robust Nonparametric Confidence Intervals for Regression-Discontinuity Designs. Econometrica 82(6): 2295-2326.
+#'
+#' Calonico, S., M. D. Cattaneo, and R. Titiunik. 2014b. Robust Data-Driven Inference in the Regression-Discontinuity Design. Stata Journal 14(4): 909-946.
+#'
+#' Calonico, S., M. D. Cattaneo, and R. Titiunik. 2015a. Optimal Data-Driven Regression Discontinuity Plots. Journal of the American Statistical Association 110(512): 1753-1769.
+#'
+#' Calonico, S., M. D. Cattaneo, and R. Titiunik. 2015b. rdrobust: An R Package for Robust Nonparametric Inference in Regression-Discontinuity Designs. R Journal 7(1): 38-51.
+#'
+#' Cattaneo, M. D., B. Frandsen, and R. Titiunik. 2015. Randomization Inference in the Regression Discontinuity Design: An Application to the Study of Party Advantages in the U.S. Senate. Journal of Causal Inference 3(1): 1-24.
+#'
+#'
+#' @export
+#'
+#' @examples
+#'
+#' X <- runif(1000, -1, 1)
+#' Y <- 10 + 5*X + 3*(X>=0)+rnorm(1000)
+#' rd_estimate(Y,X)
+#' summary(rd_estimate(Y,X))
 
-estimator()
+rd_estimate <- function(Y, X, cutpoint = NULL, bandwidth = NULL, kernel_method = "tri", bw_method = "mserd", cl = 95){
+
+  out <- rdrobust::rdrobust(y = Y, x = X, c = cutpoint, fuzzy = NULL,
+                     deriv = NULL, p = NULL, q = NULL, h = bandwidth, b = NULL,
+                     rho = NULL, covs = NULL, covs_drop = TRUE, kernel = kernel_method,
+                     weights = NULL, bwselect = bw_method, vce = "nn", cluster = NULL,
+                     nnmatch = 3, level = cl, scalepar = 1, scaleregul = 1,
+                     sharpbw = FALSE, all = NULL, subset = NULL, masspoints = "adjust",
+                     bwcheck = NULL, bwrestrict = TRUE, stdvars = FALSE)
+
+  # fill in kenrnel_method, bw_method, bandwidth, cutpoint, x, y, confidence level,
+
+return(out)
+}
